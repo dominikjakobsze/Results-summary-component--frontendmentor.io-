6b025f52b2c9c1654784e4b092213b774b338629
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-08-08 16:16:10 +0200

    error handler added!

--- frontend/app-vite/public/js/component.js
+++ frontend/app-vite/public/js/component.js
@@ -1,8 +1,17 @@
 import data from "../data/data.json";
 import DOMPurify from 'dompurify';
 
+const errorHandler = (message) => {
+    console.log("score is not a number");
+    //optional: write your logic here to inform user about the error before stopping the execution of the script
+    throw new Error(message);
+};
+
 const formatDataPoints = (data) => {
     return data.reduce((acc, curr) => {
+        if (Number(curr.score) == NaN) {
+            errorHandler("Score is not a number - data is corrupted");
+        }
         return acc + curr.score;
     }, 0);
 };
@@ -10,7 +19,10 @@ const formatDataPoints = (data) => {
 const pointsAvrage = (points,data) => {
     return Math.round(points / data.length);
 };
-const clean = DOMPurify.sanitize('<b>hello there</b>');
-console.log(clean);
 
-document.querySelectorAll("[data-counter='data-counter")[0].innerHTML = pointsAvrage(formatDataPoints(data),data);
+const clean = DOMPurify.sanitize(pointsAvrage(formatDataPoints(data),data), {ALLOWED_TAGS: [], ALLOWED_ATTR: []});
+document.querySelectorAll("[data-counter='data-counter")[0].innerHTML = clean;
+
+document.querySelectorAll("[data-counter='data-counter")[0].addEventListener("click", () => {
+    console.log("click");
+});
