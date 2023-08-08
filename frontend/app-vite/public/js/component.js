9a66c324d22f10d17de4f0701cb786e284fbbac9
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-08-08 15:57:21 +0200

    DOMPurify added

--- frontend/app-vite/public/js/component.js
+++ frontend/app-vite/public/js/component.js
@@ -1,4 +1,5 @@
 import data from "../data/data.json";
+import DOMPurify from 'dompurify';
 
 const formatDataPoints = (data) => {
     return data.reduce((acc, curr) => {
@@ -8,7 +9,8 @@ const formatDataPoints = (data) => {
 
 const pointsAvrage = (points,data) => {
     return Math.round(points / data.length);
-}
-console.log(pointsAvrage(formatDataPoints(data),data));
+};
+const clean = DOMPurify.sanitize('<b>hello there</b>');
+console.log(clean);
 
-console.log(document.querySelectorAll("[data-counter='data-counter")[0]);
+document.querySelectorAll("[data-counter='data-counter")[0].innerHTML = pointsAvrage(formatDataPoints(data),data);
