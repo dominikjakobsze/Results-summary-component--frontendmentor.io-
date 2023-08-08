dd534c63e999bfcf601bb2dba50f4fde3e6a9813
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-08-08 17:25:53 +0200

    render section in js

--- frontend/app-vite/public/js/component.js
+++ frontend/app-vite/public/js/component.js
@@ -26,3 +26,17 @@ document.querySelectorAll("[data-counter='data-counter")[0].addEventListener("cl
     //just to make sure that throw stops the execution of the script
     console.log("click");
 });
+
+const renderSummaryContainer = document.querySelector("[data-target='summary-container']");
+
+const renderSummaryItems = (data) => {
+    renderSummaryContainer.innerHTML = data.map((item) => {
+        return `<section class="p-2 w-full bg-[rgb(255,250,242)] rounded-xl flex flex-row flex-wrap gap-[4%] items-center justify-center">
+        <img class="flex-[0_0_10%] object-contain" src="${item.icon}"/>
+        <p class="text-orange-300 flex-[0_0_45%] overflow-hidden font-semibold">${item.category}</p>
+        <p class="flex-[0_0_35%] font-semibold text-gray-700">${item.score} <span class="text-gray-300">/ 100</span></p>
+        </section>`
+    }).join("");
+};
+
+renderSummaryItems(data);
