2f1c4259e274d62f8b0e90f8da13b237d17c1dc4
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-08-08 18:32:53 +0200

    new breakpoints and mobile design done

--- frontend/app-vite/tailwind.config.js
+++ frontend/app-vite/tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '550px',
+      'md': '768px',
+      'lg': '900px',
+      'xl': '1140px',
+      '2xl': '1450px',
+      '3xl': '1660px',
+    },
     extend: {},
   },
   plugins: [],
