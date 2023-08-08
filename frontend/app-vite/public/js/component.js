8dc16a22b6d93ca5085e66d20d2c8c7a5d594e5d
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-08-08 17:44:48 +0200

    rendering dong

--- frontend/app-vite/public/js/component.js
+++ frontend/app-vite/public/js/component.js
@@ -31,11 +31,16 @@ const renderSummaryContainer = document.querySelector("[data-target='summary-con
 
 const renderSummaryItems = (data) => {
     renderSummaryContainer.innerHTML = data.map((item) => {
-        return `<section class="p-2 w-full bg-[rgb(255,250,242)] rounded-xl flex flex-row flex-wrap gap-[4%] items-center justify-center">
-        <img class="flex-[0_0_10%] object-contain" src="${item.icon}"/>
-        <p class="text-orange-300 flex-[0_0_45%] overflow-hidden font-semibold">${item.category}</p>
-        <p class="flex-[0_0_35%] font-semibold text-gray-700">${item.score} <span class="text-gray-300">/ 100</span></p>
-        </section>`
+        return `
+        <section 
+            class="p-2 w-full rounded-xl flex flex-row flex-wrap gap-[4%] items-center justify-center"
+            style="background-color: ${item.bgcolorclass}; color: ${item.textcolorclass};"
+            >
+                <img class="flex-[0_0_10%] object-contain" src="${item.icon}"/>
+                <p class="flex-[0_0_45%] overflow-hidden font-semibold">${item.category}</p>
+                <p class="flex-[0_0_35%] font-semibold text-gray-700">${item.score} <span class="text-gray-300">/ 100</span></p>
+        </section>
+        `
     }).join("");
 };
 
